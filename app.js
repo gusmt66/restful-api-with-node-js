418ba22d477e71676ea6e9ddc482142ef8b0883d
Gustavo Morantes <gmorantes@merricktowle.com>
2019-04-29 17:18:41 -0400

    code cleanup

--- app.js
+++ app.js
@@ -1,51 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParse = require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI');
-const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
 
-app.use(bodyParse.urlencoded({extended:true}));
-app.use(bodyParse.json());
-
-bookRouter.route('/books')
-  //SAVE a book
-  .post((req,res)=>{
-    const book = new Book(req.body);
-    book.save();
-    console.log(book);
-    return res.status(201).json(book);
-  })
-  //GET all books
-  .get((req, res) => {
-    //const query = req.query;
-    //const {query} = req;
-    const query = {};
-    if(req.query.genre){
-      query.genre = req.query.genre;
-    }
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    });
-  });
-
-  //GET single book
-  bookRouter.route('/books/:bookId')
-  .get((req, res) => {
-
-    Book.findById(req.params.bookId, (err, book) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(book);
-    });
-  });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/api', bookRouter);
 
@@ -54,5 +18,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Running on port  ${port}`);
+  console.log(`Running on port ${port}`);
 });
